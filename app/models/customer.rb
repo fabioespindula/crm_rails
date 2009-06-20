62a7f62f66bfd981cf71367b914238ef6c344918
Fabio Espindula <fpe@punchbowlsoftware.com>
2009-06-20 14:17:36 -0400

    created the scaffold for customers and projects

---
+++ app/models/customer.rb
@@ -0,0 +1,9 @@
+class Customer < ActiveRecord::Base
+  validates_presence_of :name, :description
+  
+  validates_uniqueness_of :name
+  
+  validates_format_of :phone, :with => /^(\d|\s)*$/
+  
+  validates_inclusion_of :rating, :in => 1..5
+end
