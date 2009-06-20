9077bc94bb28604203782c1ad1c986f8fcd4b7db
Fabio Espindula <fpe@punchbowlsoftware.com>
2009-06-20 15:36:35 -0400

    eager loading

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,7 +5,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.xml
   def index
-    @projects = Project.all
+    @projects = Project.find(:all, :joins => "LEFT OUTER JOIN customers ON customers.id = projects.customer_id")
 
     respond_to do |format|
       format.html # index.html.erb
